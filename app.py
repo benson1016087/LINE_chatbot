00f2c2926b9aa8c3abb2e199fbf277042aaf5758
benson <benson1016087@gmail.com>
2021-01-22 00:01:03 +0800

    slightly modify

---
+++ app.py
@@ -0,0 +1,59 @@
+from flask import Flask, request, abort
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import *
+
+import utils 
+
+app = Flask(__name__)
+
+# Channel Access Token
+line_bot_api = LineBotApi('ZHI/OgIUx7IsyImXYg+9X6ANFG47iHuiaj6/Fn8da5cyYwPTTuGh91Egg8vvHre2k9eofGf5fuF5qhOhLklgLRg3x9xYAjQn/0FDw5j355vgPAtoT5gIxGRanTESEVBjv7mWI+IeY6oUd/LLTcU1+AdB04t89/1O/w1cDnyilFU=')
+# Channel Secret
+handler = WebhookHandler('0dcfb97e623da9942a3acb6b86e0e9a1')
+
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(401)
+    return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    input_text = event.message.text.strip()
+    
+    if input_text in ['education', 'background']:
+        text = utils.get_education()
+    elif input_text in ['cmlab', 'lab']:
+        text = utils.get_lab_msg()
+    elif input_text == 'intern':
+        text = utils.get_intern_msg()
+    elif input_text in ['skill', 'skills']:
+        text = utils.get_skill_msg()
+    elif input_text == 'contact':
+        text = utils.get_contact_msg()
+    elif input_text in ['award', 'honor']:
+        text = utils.get_award_msg()
+    else:
+        text = utils.get_default_msg()
+    message = TextSendMessage(text=text)
+    line_bot_api.reply_message(event.reply_token, message)
+
+import os
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
---
+++ utils.py
@@ -0,0 +1,79 @@
+
+def get_default_msg():
+    text = '''Hi, you can enter the keywords to acquire some informations about me:
+
+ * education, background: My education background.
+ * cmlab, lab: Information about the lab I'm currently in.
+ * intern: My past intern experience.
+ * skill(s): The CS related skills that I have.
+ * contact: The ways to contact me. 
+ * award, honor: Display the selected honors and awards
+    
+E.g. Enter 'cmlab' or 'lab' can get the information of my lab works.'''
+    
+    return text
+
+def get_education():
+    text = "I'm a senior in National Taiwan University (NTU), majoring in Computer Science & Information Engineering. Until now, my average GPA is 4.17."
+
+    return text
+
+def get_lab_msg():
+    text = "I'm currently in Communications and Multimedia Laboratory (CMLab), under the instruction of prof. Winston Hsu.\n"
+    text += '''These are topics that l've researched before:
+    * Cross-domain few-shot learning
+    * Object referring
+    * Video trope analysis (ongoing)'''
+
+    return text
+
+def get_intern_msg():
+    text = "In last two summer vacations, I've worked in Perfect Corp. and ASUS Intelligent Cloud Service Center(AICS) as an intern.\n"
+    text += "The following shows the detail work I've done:\n"
+    text += '''
+* Perfect Corp.:
+  - Period: Jul. 2019 - Aug. 2019
+  - Worked in RD-AI-AT team 
+  - Extend existing models, and deal with CV-related problem by using deep learning
+
+* AICS:
+  - Period: Jul. 2020 - Sep. 2020
+  - Worked in Medical-NLU team
+  - Add features in their medical searching engine (EMR search)
+  - Adapt NLU to help structuring medical record'''
+
+    return text
+
+def get_skill_msg():
+    text = '''Those are the CS-related skills that I have:
+
+C/C++ programming
+Python programming
+Machine learning / Deep learning
+Shell scripts
+Linux
+Basic computer security
+Basic frontend / backend'''
+
+    return text
+
+def get_contact_msg():
+    text = 'E-mail: b06902066@ntu.edu.tw\n'
+    text += 'Phone: 0963-831-276\n'
+    text += 'Linkedin: /bing-chen-tsai'
+
+    return text
+
+def get_award_msg():
+    text = "Those are the awards that I got:\n"
+    text += '''
+* Ministry of Education Intercollegiate AI CUP 2019
+  - Awards: Honorable Mention
+  - This competition is focus on thesis topic classification by utilizing NLP models.
+
+* Thematic Exhibition of Undergraduate
+  - Awards: Honorable Mention
+  - Our presentation topic: Large Margin Mechanism and Pseudo Query Set on CrossDomain Few-Shot Learning
+
+* Presidential Award
+  - In second semester of academic year 2019-2020.'''

1436042e899dd19d6858f10d89fc4269627fdb7c
benson <benson1016087@gmail.com>
2021-01-22 00:07:12 +0800

    slightly modify

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, abort
 
 from linebot import (
@@ -52,7 +53,6 @@ def handle_message(event):
     message = TextSendMessage(text=text)
     line_bot_api.reply_message(event.reply_token, message)
 
-import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
